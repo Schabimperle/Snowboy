60774753409e0b484c6af63762b25cdcc19920be
Malte Schababerle <m.schababerle@gmail.com>
2019-02-14 14:55:38 +0100

    replaced google stt with deepspeech

--- src/speech-recognizer.ts
+++ src/speech-recognizer.ts
@@ -1,24 +1,34 @@
-
-// tslint:disable-next-line
-const Stt = require("@google-cloud/speech");
-// tslint:disable-next-line
-const { Detector, Models } = require("snowboy");
+// @ts-ignore
+import * as Ds from "deepspeech";
+// @ts-ignore
+import MemoryStream from "memory-stream";
+// @ts-ignore
+import { Detector, Models } from "snowboy";
 import { Writable, WritableOptions } from "stream";
 
-const SPEECH_REQUEST = {
-    config: {
-        alternativeLanguageCodes: ["en-US"],
-        encoding: "LINEAR16",
-        languageCode: "de-DE",
-        maxAlternatives: 5,
-        sampleRateHertz: 16000,
-        speechContexts: [{
-            phrases: [] as string[],
-        }],
-    },
-    interimResults: false, // If you want interim results, set this to true
-};
 const RECORD_TIMEOUT = 1500;
+
+const DS_PATH = "models/";
+const MODEL_PATH = DS_PATH + "output_graph.rounded.pbmm";
+const ALPHABET_PATH = DS_PATH + "alphabet.txt";
+const LM_PATH = DS_PATH + "lm.binary";
+const TRIE_PATH = DS_PATH + "trie";
+
+// Beam width used in the CTC decoder when building candidate transcriptions
+const BEAM_WIDTH = 500;
+// The alpha hyperparameter of the CTC decoder. Language Model weight
+const LM_ALPHA = 0.75;
+// The beta hyperparameter of the CTC decoder. Word insertion bonus.
+const LM_BETA = 1.85;
+
+// These constants are tied to the shape of the graph used (changing them changes
+// the geometry of the first layer), so make sure you use the same constants that
+// were used during training
+
+// Number of MFCC features to use
+const N_FEATURES = 26;
+// Size of the context window used for producing timesteps in the input vector
+const N_CONTEXT = 9;
 
 export { WritableOptions } from "stream";
 
@@ -27,11 +37,10 @@ export class SpeechRecognizer extends Writable {
     private detector: any;
     private sttClient: any;
     private commands: string[];
-    private sttStream: Writable | null = null;
+    private sttStream: any | null = null;
     private timer?: NodeJS.Timeout;
-    private sttResult: boolean = false;
 
-    constructor(detectorModels: any[], googleKeysPath: string, commands: string[], streamOptions?: WritableOptions) {
+    constructor(detectorModels: any[], commands: string[], streamOptions?: WritableOptions) {
         super(streamOptions);
         const models = new Models();
         detectorModels.forEach((model) => models.add(model));
@@ -41,20 +50,17 @@ export class SpeechRecognizer extends Writable {
             models,
             resource: "node_modules/snowboy/resources/common.res",
         })
-            .on("hotword", this.handleDetectorHotword.bind(this))
-            .on("error", this.handleDetectorError.bind(this));
-        // .on('silence', this.handleDetectorSilence.bind(this))
-        // .on('sound', this.handleDetectorSound.bind(this))
-        this.sttClient = new Stt.SpeechClient({ keyFilename: googleKeysPath });
+            .on("hotword", this.onDetectorHotword.bind(this))
+            .on("error", console.error)
+            .on("silence", this.onDetectorSilence.bind(this));
+        // .on('sound', this.onDetectorSound.bind(this))
         this.commands = commands;
-        SPEECH_REQUEST.config.speechContexts[0].phrases = this.commands;
+
+        this.sttClient = new Ds.Model(MODEL_PATH, N_FEATURES, N_CONTEXT, ALPHABET_PATH, BEAM_WIDTH);
+        this.sttClient.enableDecoderWithLM(ALPHABET_PATH, LM_PATH, TRIE_PATH, LM_ALPHA, LM_BETA);
     }
 
-    // handleDetectorSilence() {
-    // 	console.debug('silence');
-    // }
-
-    // handleDetectorSound(buffer) {
+    // onDetectorSound(buffer) {
     // 	// <buffer> contains the last chunk of the audio that triggers the "sound"
     // 	// event. It could be written to a wav Stream.
     // 	console.debug('sound');
@@ -64,18 +70,11 @@ export class SpeechRecognizer extends Writable {
         return this.sttStream;
     }
 
-    public endTranscribing() {
-        if (this.sttStream && this.sttStream.writable) {
-            this.sttStream.end();
-        }
-        this.sttStream = null;
-    }
-
     public _write(chunk: any, encoding: string, callback: (error?: Error | null) => void) {
         if (this.sttStream) {
             this.sttStream.write(chunk, encoding, callback);
             if (this.timer) { clearTimeout(this.timer); }
-            this.timer = setTimeout(this.endTranscribing.bind(this), RECORD_TIMEOUT);
+            this.timer = setTimeout(this.onTimeout.bind(this), RECORD_TIMEOUT);
         } else {
             this.detector.write(chunk, encoding, callback);
         }
@@ -87,7 +86,16 @@ export class SpeechRecognizer extends Writable {
         callback();
     }
 
-    private handleDetectorHotword(index: number, hotword: string, buffer: Buffer) {
+    private onTimeout() {
+        if (this.sttStream && this.sttStream.writable) {
+            this.sttStream.end();
+            this.transcribe();
+        }
+        // make _write calls go to hotword detection again
+        this.sttStream = null;
+    }
+
+    private onDetectorHotword(index: number, hotword: string, buffer: Buffer) {
         // <buffer> contains the last chunk of the audio that triggers the "hotword"
         // event. It could be written to a wav Stream. You will have to use it
         // together with the <buffer> in the "sound" event if you want to get audio
@@ -95,58 +103,37 @@ export class SpeechRecognizer extends Writable {
         console.debug("hotword", index, hotword);
         this.emit("hotword");
 
-        this.sttStream = this.sttClient.streamingRecognize(SPEECH_REQUEST)
-            // .on('start', this.handleSttStart.bind(this))
-            .on("data", this.handleSttData.bind(this))
-            .on("error", this.handleSttError.bind(this))
-            .on("end", this.handleSttEnd.bind(this));
-        this.timer = setTimeout(this.endTranscribing.bind(this), RECORD_TIMEOUT);
+        this.timer = setTimeout(this.onTimeout.bind(this), 3000);
         this.emit("transcribe-start");
+
+        this.sttStream = new MemoryStream();
     }
 
-    private handleSttData(data: any) {
-        this.sttResult = true;
-        if (data.error) {
-            this.handleSttError(data.error);
+    private onDetectorSilence() {
+        console.debug("silence");
+    }
+
+    private transcribe() {
+        if (!this.sttStream) {
             return;
         }
-        if (!data.results.length || !data.results[0].alternatives) {
-            return;
-        }
-        const alternatives = data.results[0].alternatives;
-        // if any transcript starts with a command emit an appropriate event
-        for (let i = 0; i < alternatives.length; i++) {
-            console.debug(`transcript (${i}/${alternatives.length}):`, alternatives[i].transcript);
-            for (const command of this.commands) {
-                if (alternatives[i].transcript.toLowerCase().startsWith(command)) {
-                    this.emit("command", command, alternatives[i].transcript.slice(command.length + 1));
-                    return;
-                }
+
+        // transcribe
+        const audioBuffer = this.sttStream.toBuffer();
+        const result = this.sttClient.stt(audioBuffer.slice(0, audioBuffer.length / 2), 16000);
+        console.debug("transcript:", result);
+
+        // check for matching commands
+        for (const cmd of this.commands) {
+            if (result.toLowerCase().startsWith(cmd)) {
+                this.emit("command", cmd, cmd.slice(cmd.length + 1));
+                return;
             }
         }
-        const text: string = alternatives[0].transcript;
-        const command = text.substr(0, text.indexOf(" ") || text.length);
-        const rest = text.slice(command.length + 1);
+
+        // we dont listen to that command -> emit event 'bad-command'
+        const command = result.replace(/ .*/, "");
+        const rest = result.slice(command.length + 1);
         this.emit("bad-command", command, rest);
-    }
-
-    private handleSttEnd() {
-        if (!this.sttResult) {
-            this.emit("bad-command", "none", "");
-        }
-
-        this.sttResult = false;
-        this.sttStream = null;
-        this.emit("transcribe-end");
-    }
-
-    private handleSttError(error: any) {
-        console.error(error);
-        this.sttStream = null;
-        this.emit("transcribe-error", error);
-    }
-
-    private handleDetectorError(error: any) {
-        console.dir(error);
     }
 }
